5924c3b92daa9932a4cb55cc628c7ac7ff0fdb33
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-11 20:55:57 -0300

    Camera object implemented

---
+++ include/Camera.hpp
@@ -0,0 +1,29 @@
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Camera {
+  private:
+    sf::View view_;
+
+    float screen_width_;
+    float screen_height_;
+    float level_width_;
+    float level_height_;
+    float zoom_factor_;
+
+  public:
+    Camera(float screen_width, float screen_height, float level_width, float level_height);
+
+    void SetZoomFactor(float zoom_factor) {
+      zoom_factor_ = zoom_factor;
+      view_.zoom(zoom_factor);
+    }
+
+    void Update(const sf::Vector2f& player_position);
+
+    void Apply(sf::RenderWindow& window);
+};
+
+#endif
---
+++ src/Camera.cpp
@@ -0,0 +1,36 @@
+#include "Camera.hpp"
+
+Camera::Camera(float screen_width, float screen_height, float level_width, float level_height) : screen_width_(screen_width), screen_height_(screen_height), level_width_(level_width), level_height_(level_height), zoom_factor_(1.0f) {
+  view_.setSize(screen_width, screen_height);
+  view_.setCenter(screen_width / 2, screen_height / 2);
+}
+
+void Camera::Update(const sf::Vector2f& player_position) {
+  float camera_x = player_position.x;
+  float camera_y = player_position.y;
+
+  float x = screen_width_ * zoom_factor_ / 2;
+  float y = screen_height_ * zoom_factor_ / 2;
+
+  if (camera_x < x) {
+    camera_x = x;
+  }
+
+  if (camera_y < y) {
+    camera_y = y;
+  }
+
+  if (camera_x > level_width_ - x) {
+    camera_x = level_width_ - x;
+  }
+
+  if (camera_y > level_height_ - y) {
+    camera_y = level_height_ - y;
+  }
+
+  view_.setCenter(camera_x, camera_y);
+}
+
+void Camera::Apply(sf::RenderWindow& window) {
+  window.setView(view_);
+}
