b7781133766045f910a44bff21be2fffca6e76cf
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-11 20:55:17 -0300

    utils to draw tiles

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -3,4 +3,17 @@
 
 #include "Map.hpp"
 #include <vector>
+
+// Brute force xD
+
+void PlacePipe(std::vector<std::vector<int>>& level, int top, int left, int height);
+
+void PlaceGround(std::vector<std::vector<int>>& level, int top, int left, int right, int height, int gid);
+
+void PlaceHole(std::vector<std::vector<int>>& level, int top, int left, int width);
+
+void PlaceAlternate(std::vector<std::vector<int>>& level, int left, int width, int height);
+
+std::vector<std::vector<int>> MakeLevel1();
+
 #endif
