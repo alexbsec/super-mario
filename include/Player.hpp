cb3b3e33d94ac756d8c5324e9835a47e595d3f81
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-11 12:15:18 -0300

    added horizontal collision and better y collision + adjusted speed

--- include/Player.hpp
+++ include/Player.hpp
@@ -2,18 +2,18 @@
 #define PLAYER_HPP
 
 #include <SFML/Graphics.hpp>
+#include "Physics.hpp"
 
 class Player {
   private:
     sf::Texture player_texture_;
     sf::Sprite player_sprite_;
-    float velocity_y_;
-    bool is_jumping_;
+    BodyPhysics body_;
 
   public:
     Player(const std::string& texture_file);
     void HandleInput();
-    void Update();
+    void Update(const std::vector<sf::FloatRect>& collision_bounds);
     void Draw(sf::RenderWindow& window);
 };
 
