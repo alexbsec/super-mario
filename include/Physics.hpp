827d090301a81fb5d4401fc8f07e306a52277e46
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-12 19:04:54 -0300

    Enemy physics struct added

--- include/Physics.hpp
+++ include/Physics.hpp
@@ -1,13 +1,25 @@
 #ifndef PHYSICS_HPP
 #define PHYSICS_HPP
 
+struct EnemyPhysics {
+  float gravity;
+  float terminal_velocity;
+  float x_speed;
+  float y_speed;
+  bool is_moving_left;
+};
+
 struct BodyPhysics {
   float gravity;
   float terminal_velocity;
   float x_speed;
   float y_speed;
   float jump_speed;
+  float acceleration;
+  float max_x_speed;
   bool is_jumping;
+  bool is_accelerating;
+  bool has_jumped;
 };
 
 #endif
