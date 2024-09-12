983b5c2594157d95f1ca3e62d8ff2183279ecba1
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-12 19:06:16 -0300

    implementation of the background member function

--- src/Map.cpp
+++ src/Map.cpp
@@ -32,6 +32,34 @@ Map::Map(const std::string& tileset_path) : tiles_({}) {
       }
 }
 
+void Map::LoadBackground(
+  const std::string& tileset,
+  const std::vector<std::vector<int>>& bg
+) {
+  const int tile_size = 16;
+
+  for (size_t y = 0; y < bg.size(); y++) {
+    for (size_t x = 0; x < bg[y].size(); x++) {
+      int tile_type = bg[y][x];
+      sf::IntRect texture_rect;
+      bool is_solid = false;
+      TileType behavior = TileType::SIMPLE;
+
+      switch (tile_type) {
+        case 1:
+          texture_rect = sf::IntRect(3 * tile_size, 21 * tile_size, tile_size, tile_size);
+          break;
+        case 0:
+          continue;
+      }
+
+      Tile tile(tile_type, texture_rect, is_solid, behavior, tileset_texture_);
+      tile.GetSprite().setPosition(x * tile_size, y * tile_size);
+      bg_tiles_.push_back(tile);
+    }
+  }
+}
+
 void Map::LoadMap(
   const std::string& tileset,
   const std::vector<std::vector<int>>& level_data
@@ -117,6 +145,11 @@ void Map::LoadMap(
           behavior = TileType::INTERACTABLE;
           break;
 
+        case 11:
+          // blue sky bg
+          texture_rect = sf::IntRect(3 * tile_size, 21 * tile_size, tile_size, tile_size);
+          is_solid = false;
+          behavior = TileType::SIMPLE;
         case 0:
           continue;
       }
@@ -126,6 +159,12 @@ void Map::LoadMap(
 
       tiles_.push_back(tile);
     }
+  }
+}
+
+void Map::DrawBackground(sf::RenderWindow& window) {
+  for (const auto& tile : bg_tiles_) {
+    window.draw(tile.GetSprite());
   }
 }
 
