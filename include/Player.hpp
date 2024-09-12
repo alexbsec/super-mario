1a5b075643bd8aadb66721caeda6959cf820198b
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-12 19:05:22 -0300

    fixes to prevent low fps (added delta time calculation and better values for speed/acceleration)

--- include/Player.hpp
+++ include/Player.hpp
@@ -10,12 +10,17 @@ class Player {
     sf::Sprite player_sprite_;
     BodyPhysics body_;
 
+    void CheckXMovement_();
+
   public:
     Player(const std::string& texture_file);
     sf::Vector2f GetPosition() const;
-    void HandleInput();
-    void Update(const std::vector<sf::FloatRect>& collision_bounds);
+    void HandleInput(const sf::Event& event, float delta_time);
+    void Update(const std::vector<sf::FloatRect>& collision_bounds, float delta_time);
     void Draw(sf::RenderWindow& window);
+    sf::FloatRect GetCollisionBounds() const;
+    float GetYSpeed() const;
+    void Bounce();
 };
 
 #endif
