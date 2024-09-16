72e76f0ec01e18554e21cb0d1f25f46adb39c21d
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-16 10:15:31 -0300

    now game loop goes forever until player closes the window

--- src/main.cpp
+++ src/main.cpp
@@ -6,112 +6,37 @@
 #include "Utils.hpp"
 #include "Camera.hpp"
 #include "Enemy.hpp"
-
-const int TILE_SIZE = 16;
-const int TILES_X = 16;
-const int TILES_Y = 15;
-
-Map GetTestMap() {
-  std::vector<std::vector<int>> level_data = MakeLevel1();
-  Map map("assets/lv1-tileset.png");
-  map.LoadMap("assets/lv1-tileset.png", level_data);
-
-  return map;
-}
-
-Map GetBackground() {
-  std::vector<std::vector<int>> bg = MakeBackground();
-  Map map("assets/lv1-tileset.png");
-  map.LoadBackground("assets/lv1-tileset.png", bg);
-
-  return map;
-}
+#include "Game.hpp"
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(600, 300), "Super Mario");
+  sf::RenderWindow window(sf::VideoMode(600, 400), "Super Mario");
 
+  while (true) {
+    std::vector<std::unique_ptr<Enemy>> enemies;
+    std::vector<sf::FloatRect> mario_collision_bounds, enemy_collision_bounds;
 
-  sf::Vector2u window_size = window.getSize();
-  float scale_x = static_cast<float>(window_size.x) / (TILES_X * TILE_SIZE);
-  float scale_y = static_cast<float>(window_size.y) / (TILES_Y * TILE_SIZE);
+    Camera camera = MakeCamera(window, 0.8f);
 
-  Camera camera(scale_x * TILE_SIZE * TILES_X, scale_y * TILE_SIZE * TILES_Y, 224 * TILE_SIZE, 37 * TILE_SIZE);
+    enemies = SetEnemies();
 
-  std::vector<std::unique_ptr<Enemy>> enemies;
-  std::vector<sf::FloatRect> mario_collision_bounds, enemy_collision_bounds;
+    Player mario("assets/MarioIdle.png");
+    Map map = GetTestMap();
+    Map bg = GetBackground();
 
-  enemies = SetEnemies();
-  //camera.SetZoomFactor(0.5f);
+    sf::Clock clock;
 
-  if (enemies.empty()) std::cout << "ops" << std::endl;
-  Player mario("assets/MarioIdle.png");
-  Map map = GetTestMap();
-  Map bg = GetBackground();
+    if (!GameLoop(
+      window,
+      clock,
+      mario,
+      enemies,
+      camera,
+      map,
+      bg,
+      mario_collision_bounds,
+      enemy_collision_bounds
+    )) break;
 
-  sf::Clock clock;
-  // Main game loop
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
-    }
-
-    mario_collision_bounds.clear();
-    enemy_collision_bounds.clear();
-
-    mario_collision_bounds = map.GetCollisionBounds();
-    enemy_collision_bounds = map.GetCollisionBounds();
-    enemy_collision_bounds.push_back(mario.GetCollisionBounds());
-
-    sf::Time deltaTime = clock.restart();
-
-    for (auto& enemy : enemies) {
-      mario_collision_bounds.push_back(enemy->GetBounds());
-      enemy->Update(enemy_collision_bounds, deltaTime.asSeconds());
-    }
-
-    enemies.erase(
-      std::remove_if(enemies.begin(), enemies.end(), [](std::unique_ptr<Enemy>& enemy) {
-        return enemy->IsDead();
-      }),
-      enemies.end()
-   );
-
-    mario.HandleInput(event, deltaTime.asSeconds());
-    mario.Update(map.GetCollisionBounds(), deltaTime.asSeconds());
-
-    for (auto& enemy : enemies) {
-      sf::FloatRect mario_bounds = mario.GetCollisionBounds();
-      sf::FloatRect enemy_bounds = enemy->GetBounds();
-
-      if (mario_bounds.intersects(enemy_bounds)) {
-        if (mario.GetYSpeed() > 0 && (mario_bounds.top + mario_bounds.height) <= (enemy_bounds.top + enemy_bounds.height * 0.2f)) {
-          enemy->OnJumpedOn();
-          mario.Bounce();
-        }
-      }
-    }
-
-    camera.Update(mario.GetPosition());
-
-    window.clear();
-
-    camera.Apply(window);
-    bg.DrawBackground(window);
-    map.Draw(window);
-    mario.Draw(window);
-
-
-    for (auto& enemy : enemies) {
-      enemy->Draw(window);
-    }
-
-
-
-
-    window.display();
   }
 
   return EXIT_SUCCESS;
