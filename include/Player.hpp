4613cc700fd399aa37082321fd3944eb86b74a42
alexbsec <j.a.c.buschinelli@gmail.com>
2024-09-16 10:13:46 -0300

    changes to player i dont really remember

--- include/Player.hpp
+++ include/Player.hpp
@@ -9,8 +9,13 @@ class Player {
     sf::Texture player_texture_;
     sf::Sprite player_sprite_;
     BodyPhysics body_;
+    bool is_dead_;
+    bool has_died_;
+    float grace_time_;
+    bool jump_dead_;
 
     void CheckXMovement_();
+    void PlayDeathAnimation_(float delta_time);
 
   public:
     Player(const std::string& texture_file);
@@ -21,6 +26,11 @@ class Player {
     sf::FloatRect GetCollisionBounds() const;
     float GetYSpeed() const;
     void Bounce();
+    bool IsDead() const;
+    void Die(bool jump);
+    void SetGracePeriod(float time);
+    bool IsInGrace() const;
+    void Reset();
 };
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -7,7 +7,21 @@ void Player::CheckXMovement_() {
 
 }
 
-Player::Player(const std::string& texture_file) {
+void Player::PlayDeathAnimation_(float delta_time) {
+  if (!has_died_ && jump_dead_) {
+    has_died_ = true;
+    body_.x_speed = 0;
+    body_.y_speed = -body_.jump_speed;
+  }
+
+  body_.y_speed += body_.gravity * delta_time;
+  if (body_.y_speed >= body_.terminal_velocity) {
+     body_.y_speed = body_.terminal_velocity;
+  }
+  player_sprite_.move(0.0f, body_.y_speed * delta_time);
+}
+
+Player::Player(const std::string& texture_file) : is_dead_(false), has_died_(false), grace_time_(0.0f), jump_dead_(true) {
   if (!player_texture_.loadFromFile(texture_file)) {
     // handle
   }
@@ -33,99 +47,109 @@ sf::Vector2f Player::GetPosition() const {
 }
 
 void Player::HandleInput(const sf::Event& event, float delta_time) {
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
-      if (body_.x_speed > -100.0f) body_.x_speed = -100.0f;
-      body_.x_speed -= body_.acceleration * delta_time;
-      body_.is_accelerating = true;
-      if (body_.x_speed < -body_.max_x_speed) {
-        body_.x_speed = -body_.max_x_speed;
+  if (!is_dead_) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
+        if (body_.x_speed > -100.0f) body_.x_speed = -100.0f;
+        body_.x_speed -= body_.acceleration * delta_time;
+        body_.is_accelerating = true;
+        if (body_.x_speed < -body_.max_x_speed) {
+          body_.x_speed = -body_.max_x_speed;
+        }
+      } else {
+        body_.is_accelerating = false;
+        body_.x_speed = -100.0f;
       }
-    } else {
-      body_.is_accelerating = false;
-      body_.x_speed = -100.0f;
     }
-  }
 
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
-      if (body_.x_speed < 100.0f) body_.x_speed = 100.0f;
-      body_.x_speed += body_.acceleration * delta_time;
-      body_.is_accelerating = true;
-      if (body_.x_speed > body_.max_x_speed) {
-        body_.x_speed = body_.max_x_speed;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
+        if (body_.x_speed < 100.0f) body_.x_speed = 100.0f;
+        body_.x_speed += body_.acceleration * delta_time;
+        body_.is_accelerating = true;
+        if (body_.x_speed > body_.max_x_speed) {
+          body_.x_speed = body_.max_x_speed;
+        }
+      } else {
+        body_.is_accelerating = false;
+        body_.x_speed = 100.0f;
       }
-    } else {
-      body_.is_accelerating = false;
-      body_.x_speed = 100.0f;
     }
-  }
 
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && event.key.code == sf::Keyboard::Up) {
-    if (!body_.is_jumping && !body_.has_jumped) {
-      body_.y_speed = -body_.jump_speed;
-      body_.is_jumping = true;
-      body_.has_jumped = true;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && event.key.code == sf::Keyboard::Up) {
+      if (!body_.is_jumping && !body_.has_jumped) {
+        body_.y_speed = -body_.jump_speed;
+        body_.is_jumping = true;
+        body_.has_jumped = true;
+      }
+    } else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+      body_.has_jumped = false;
     }
-  } else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-    body_.has_jumped = false;
   }
 }
 
 void Player::Update(const std::vector<sf::FloatRect>& collision_bounds, float delta_time) {
-  // Gravity
-  body_.y_speed += delta_time * body_.gravity;
-  if (body_.y_speed >= body_.terminal_velocity) {
-    body_.y_speed = body_.terminal_velocity;
-  }
-
-  player_sprite_.move(0.0f, body_.y_speed * delta_time);
-
-  // Horizontal movement check (left-right)
-  sf::Vector2f player_position = player_sprite_.getPosition();
-  sf::FloatRect player_bounds = player_sprite_.getGlobalBounds();
-
-    // Vertical movement check (up-down)
-  for (const auto& bound : collision_bounds) {
-    if (player_sprite_.getGlobalBounds().intersects(bound)) {
-      if (body_.y_speed > 0) {
-        player_sprite_.setPosition(player_position.x, bound.top - player_bounds.height);
-        body_.y_speed = 0;
-        body_.is_jumping = false;
-      } else if (body_.y_speed < 0) {
-        player_sprite_.setPosition(player_position.x, bound.top + bound.height);
-        body_.y_speed = 0;
-      }
-      break;
+  // Apply only if player is alive
+  if (!is_dead_) {
+    if (grace_time_ > 0.0f) {
+      grace_time_ -= delta_time;
     }
-  }
-
-  player_sprite_.move(body_.x_speed * delta_time, 0.0f);
-
-  player_position = player_sprite_.getPosition();
-  player_bounds = player_sprite_.getGlobalBounds();
-
-
-  for (const auto& bound : collision_bounds) {
-    if (player_bounds.intersects(bound)) {
-      // Move back if a collision is detected
-      if (body_.x_speed > 0) {
-        if (player_bounds.top + player_bounds.height > bound.top && player_bounds.top < bound.top + bound.height) {
-          player_sprite_.setPosition(bound.left - player_bounds.width, player_position.y);
-        }
-      } else if (body_.x_speed < 0) {
-        if (player_bounds.top + player_bounds.height > bound.top && player_bounds.top < bound.top + bound.height) {
-          player_sprite_.setPosition(bound.left + bound.width, player_position.y);
-        }
-      }
-      body_.x_speed = 0;
-      break;
+    // Gravity
+    body_.y_speed += delta_time * body_.gravity;
+    if (body_.y_speed >= body_.terminal_velocity) {
+      body_.y_speed = body_.terminal_velocity;
     }
+
+    player_sprite_.move(0.0f, body_.y_speed * delta_time);
+
+    // Horizontal movement check (left-right)
+    sf::Vector2f player_position = player_sprite_.getPosition();
+    sf::FloatRect player_bounds = player_sprite_.getGlobalBounds();
+
+      // Vertical movement check (up-down)
+    for (const auto& bound : collision_bounds) {
+      if (player_sprite_.getGlobalBounds().intersects(bound)) {
+        if (body_.y_speed > 0) {
+          player_sprite_.setPosition(player_position.x, bound.top - player_bounds.height);
+          body_.y_speed = 0;
+          body_.is_jumping = false;
+        } else if (body_.y_speed < 0) {
+          player_sprite_.setPosition(player_position.x, bound.top + bound.height);
+          body_.y_speed = 0;
+        }
+        break;
+      }
+    }
+
+    player_sprite_.move(body_.x_speed * delta_time, 0.0f);
+
+    player_position = player_sprite_.getPosition();
+    player_bounds = player_sprite_.getGlobalBounds();
+
+
+    for (const auto& bound : collision_bounds) {
+      if (player_bounds.intersects(bound)) {
+        // Move back if a collision is detected
+        if (body_.x_speed > 0) {
+          if (player_bounds.top + player_bounds.height > bound.top && player_bounds.top < bound.top + bound.height) {
+            player_sprite_.setPosition(bound.left - player_bounds.width, player_position.y);
+          }
+        } else if (body_.x_speed < 0) {
+          if (player_bounds.top + player_bounds.height > bound.top && player_bounds.top < bound.top + bound.height) {
+            player_sprite_.setPosition(bound.left + bound.width, player_position.y);
+          }
+        }
+        body_.x_speed = 0;
+        break;
+      }
+    }
+
+    CheckXMovement_();
+  } else {
+    // Player is dead, play animation
+    PlayDeathAnimation_(delta_time);
   }
-
-  CheckXMovement_();
-
- }
+}
 
 void Player::Draw(sf::RenderWindow& window) {
   window.draw(player_sprite_);
@@ -143,4 +167,30 @@ void Player::Bounce() {
   body_.y_speed -= body_.jump_speed;
   body_.is_jumping = true;
   body_.has_jumped = true;
+}
+
+bool Player::IsDead() const {
+  return is_dead_;
+}
+
+void Player::Die(bool jump = true) {
+  is_dead_ = true;
+  jump_dead_ = jump;
+}
+
+void Player::SetGracePeriod(float time) {
+  grace_time_ = time;
+}
+
+bool Player::IsInGrace() const {
+  return grace_time_ > 0.0f;
+}
+
+void Player::Reset() {
+  player_sprite_.setPosition(10, 50);
+  body_.x_speed = 0.0f;
+  body_.y_speed = 0.0f;
+  body_.is_jumping = false;
+  body_.has_jumped = false;
+  is_dead_ = false;
 }
